e95c6e6f3d63f9aac2b4787415dad03ca71ee55b
gta191977649 <episodes27@gmail.com>
2018-11-02 02:49:49 +1100

    setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		rigidbody = gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
+		rigidbody.AddForce(0,200,500);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
